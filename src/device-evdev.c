48f9b2a36b9d1c9f8630e717f2c9fbf9c7bb2c90
polyphemus <rolfmorel@gmail.com>
2013-08-01 13:10:41 +0200

    remove double errno.h include

--- src/device-evdev.c
+++ src/device-evdev.c
@@ -17,7 +17,6 @@ You should have received a copy of the GNU Lesser General Public License
 along with libspacemouse.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include <errno.h>
 #include <unistd.h>
 #include <string.h>
 #include <fcntl.h>
