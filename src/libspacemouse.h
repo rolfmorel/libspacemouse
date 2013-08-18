8bfd9b05e3dc256d75b7801edeaf2e5e8b71a9b2
polyphemus <rolfmorel@gmail.com>
2013-08-18 22:59:42 +0200

    Add primative doxygen documentation to header

--- src/libspacemouse.h
+++ src/libspacemouse.h
@@ -17,6 +17,8 @@ You should have received a copy of the GNU Lesser General Public License
 along with libspacemouse.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+/** @file */
+
 #ifndef _LIBSPACEMOUSE_H_
 #define _LIBSPACEMOUSE_H_
 
@@ -63,106 +65,240 @@ typedef union spacemouse_event {
   struct spacemouse_event_led led;
 } spacemouse_event;
 
+/**
+ * Opaque structure representing a spacemouse device
+ */
 struct spacemouse;
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-/* Returns the head of the internal spacemouse list, use
- * spacemouse_device_list_update() at least once to initialize list.
+/**
+ * Return the head of the internal device list.
+ *
+ * Use spacemouse_device_list_update() at least once to initialize the list.
+ *
+ * @return First device in list or NULL, when NULL check errno for errors.
  */
 struct spacemouse *spacemouse_device_list(void);
 
-/* Discover devices and initialize/update the internal spacemouse list. When
- * the monitor functions are used this should only be needed to collect an
- * initial list. Returns head of internal spacemouse list.
+/**
+ * Discover devices and initialize/update the internal device list.
+ *
+ * When the monitor functions are used this function should only be called once
+ * to initialize the internal list.
+ *
+ * @return Head of internal device list or NULL, when NULL check errno for
+ * errors.
  */
 struct spacemouse *spacemouse_device_list_update(void);
 
-/* Returns next node in the spacemouse list, this is needed to cycle over all
- * connected devices. Returns NULL when there are no more new nodes.
+/**
+ * Returns next node in the device list.
+ *
+ * Call this function successively to iterate over all devices in the internal
+ * list.
+ *
+ * @param mouse The device of which the next device is to be returned.
+ *
+ * @return Next device in internal list or NULL. If NULL check errno for
+ * errors, else reached end of list.
  */
 struct spacemouse *spacemouse_device_list_get_next(struct spacemouse *mouse);
 
+/**
+ * Macro which expands to a convient for loop to iterate over the device list.
+ */
 #define spacemouse_device_list_foreach(list_iter, list_head) \
   for (list_iter = list_head; \
        list_iter != NULL; \
        list_iter = spacemouse_device_list_get_next(list_iter))
 
-/* Opens a connection to system device manager (udev with filter to input
- * devices on Linux). Returns file descriptor.
+/**
+ * Open a connection to system device manager.
+ *
+ * On Linux open a connection to udev with a filter to input devices.
+ *
+ * @return File descriptor of the opened connection or -1 in case of error.
  */
 int spacemouse_monitor_open(void);
 
-/* Wraps system device manager connection. action will be one of
- * SPACEMOUSE_ACTION_*, or -1 in case of error. If action is ADD returned
- * pointer is the new device in the spacemouse list. If action is REMOVE
- * returned pointer is to the cached spacemouse structure which was removed
- * from the spacemouse list. The cached spacemouse structure is only valid
- * until the next device removal. Blocks on read, use select, poll, etc.
+/**
+ * Wrap system device manager connection.
+ *
+ * Wrap all reads necessary to monitor device connects and disconnects.
+ *
+ * When a device connects return the new device in the device list.
+ * When a device disconnects return a cached device which is now removed from
+ * the device list.
+ *
+ * The cached device is only valid until the next device removal.
+ *
+ * @param[out] action One of SPACEMOUSE_ACTION_*, or -1 in case of error.
+ *
+ * @return If action is ADD, pointer to the new device in the spacemouse list.
+ * If action is REMOVE, pointer to the cached device. If action is IGNORE,
+ * return should be ignored.
+ *
+ * @note Blocks on read, use select, poll, etc.
  */
 struct spacemouse *spacemouse_monitor(int *action);
 
-/* Closes the system device manager connection.
+/**
+ * Close the system device manager connection.
+ *
+ * @return 0 on succes or -1 in case of error.
  */
 int spacemouse_monitor_close(void);
 
-/* Opens the device node in /dev for the mouse device.
- * Sets mouse's file descriptor and returns it or -1 in case of error.
+/**
+ * Open the device node for the device.
+ *
+ * Set device's file descriptor.
+ *
+ * @param mouse The device who's device node is to be opened.
+ *
+ * @return device's opened file descriptor or -1 in case of error.
  */
 int spacemouse_device_open(struct spacemouse *mouse);
 
-/* Wraps system HID protocol (evdev on Linux). Reads on mouse's file
- * descriptor (it blocks on read, use select, poll, etc.). Returns one of
- * SPACEMOUSE_READ_*. IGNORE when receiving an unuseful event. event is only
- * valid on SUCCESS. Returns -1 on error.
+/**
+ * Wrap system HID protocol.
+ *
+ * On Linus uses evdev, the kernel's input-subsystem.
+ *
+ * Wrap all reads on device's file descriptor.
+ *
+ * @param mouse The device of which the file descriptor is to be read for new
+ * events.
+ * @param[out] event Event which on SUCCESS is set to it's new value and
+ * otherwise is undefined.
+ *
+ * @return One of SPACEMOUSE_READ_*. IGNORE when receiving an unuseful event,
+ * -1 on error.
+ *
+ * @note Blocks on read, use select, poll, etc.
  */
 int spacemouse_device_read_event(struct spacemouse *mouse,
                                  spacemouse_event *event);
 
-/* Returns unique id for mouse device. The id that a device gets assigned is
- * an incremented number relative to previously assigned devices.
+/**
+ * Return unique id of device.
+ *
+ * The id that a device gets assigned is an incremented number relative to
+ * previously assigned devices.
  * Ids start at integer value 1.
+ *
+ * @param mouse The device of which the id is to be returned.
+ *
+ * @return The device's unique id.
  */
 int spacemouse_device_get_id(struct spacemouse *mouse);
 
-/* Returns file descriptor that was opened by spacemouse_device_open().
- * Unopened/closed devices have their the file descriptor set to -1.
+/**
+ * Return file descriptor that was opened by spacemouse_device_open(mouse).
+ *
+ * @param mouse The device of which the file descriptor is to be returned.
+ *
+ * @return The device's file descriptor or -1 in case no valid file descriptor
+ * is set.
+ *
+ * @note Unopened/closed devices have their the file descriptor set to -1.
  */
 int spacemouse_device_get_fd(struct spacemouse *mouse);
 
-/* Returns character string which is the path to the associated node in /dev.
- * This string is only valid as long as the device is still connected.
+/**
+ * Return path of the device node of the device.
+ *
+ * @param mouse The device of which the device node path is to be returned.
+ *
+ * @return The character string which holds the path of the mouse device or
+ * NULL when the path is not available.
+ *
+ * @note The returned string is only valid as long as the device is still
+ * connected.
  */
 char const * const spacemouse_device_get_devnode(struct spacemouse *mouse);
 
-/* Returns character string which is the manufacturer string associated with
- * the device. This string is only valid as long as the device is still
+/**
+ * Return manufacturer string of the device.
+ *
+ * @param mouse The device of which the manufacturer string is to be returned.
+ *
+ * @return The character string which holds manufacturer name of the mouse
+ * device or NULL when the name is not available.
+ *
+ * @note The returned string is only valid as long as the device is still
  * connected.
  */
 char const * const spacemouse_device_get_manufacturer(
     struct spacemouse *mouse);
 
-/* Returns character string which is the product string associated with the
- * device. This string is only valid as long as the device is still connected.
+/**
+ * Return product string of the device.
+ *
+ * @param mouse The device of which the product string is to be returned.
+ *
+ * @return The character string which holds product name of the mouse device or
+ * NULL when the name is not available.
+ *
+ * @note The returned string is only valid as long as the device is still
+ * connected.
  */
 char const * const spacemouse_device_get_product(struct spacemouse *mouse);
 
+/**
+ * Return maximux deviation possible on a axis, valid for all axes of device.
+ *
+ * @param mouse The device of which the maximum deviation is to be returned.
+ *
+ * @return The maximum deviation for all axes of device, or -1 in case of
+ * error, i.e. device not opened.
+ */
 int spacemouse_device_get_max_axis_deviation(struct spacemouse *mouse);
 
+/**
+ * Set grab status of device.
+ *
+ * On Linux requests to be exclusive resipient of events of device on the evdev
+ * interface.
+ *
+ * @param mouse The device of which the grab status is to be set.
+ * @param grab The status to which the device it to be set, 1 for grab, 0 for
+ * release grab.
+ *
+ * @return 0 on success, -1 in case of error.
+ */
 int spacemouse_device_set_grab(struct spacemouse *mouse, int grab);
 
-/* Get state of led of mouse device. 1 if on, 0 if off, or -1 in case of
- * error.
+/**
+ * Get state of led of device.
+ *
+ * @param mouse The device of which the led state is to be returned.
+ *
+ * @return 1 if on, 0 if off, or -1 in case of error.
  */
 int spacemouse_device_get_led(struct spacemouse *mouse);
 
-/* Set state of led of mouse device. Return -1 in case of error.
+/**
+ * Set state of led of mouse device.
+ *
+ * @param mouse The device of which the led state is to be set.
+ * @param state The state to which the led is to be set, 1 for on, 0 for off.
+ *
+ * @return 0 on success, or -1 in case of error.
  */
 int spacemouse_device_set_led(struct spacemouse *mouse, int state);
 
-/* Close the mouse's file descriptor and set it internally to -1.
+/**
+ * Close the device's file descriptor.
+ *
+ * And set it internally to -1.
+ *
+ * @param mouse The device of which the file descriptor is to be closed.
+ *
+ * @return 0 on success, or -1 in case of error.
  */
 int spacemouse_device_close(struct spacemouse *mouse);
 
